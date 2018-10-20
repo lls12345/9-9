c9126e73c893532f72535d039a75b7a61dfe3a3b
lls12345 <44298274+lls12345@users.noreply.github.com>
2018-10-20 12:53:24 +0800

    Delete 99.py

--- 99.py
+++
@@ -1,6 +0,0 @@
-for n in range(1, 10):
-    for m in range(1, n + 1):
-        if m < n:
-            print('{}*{}={}'.format(m, n, n * m, ), end=' ')
-        else:
-            print('{}*{}={}'.format(m, n, n * m))
